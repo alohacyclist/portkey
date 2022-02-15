a67aacb8511756f7f24eaa19139be92a2287a115
[Christian Mueller] <[c-mueller87@live.de]>
2022-02-15 20:51:22 +0100

    add user signup

--- models/user.model.js
+++ models/user.model.js
@@ -1,20 +1,20 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-    displayName: {
+    displayName: String,
+    firstName: String,
+    lastName: String,
+    username: {
         type: String,
-    },
-    firstName: {
-        type: String,
-    },
-    lastName: { type: String,
+        required: true,
     },
     email: {
         type: String,
         required: true,
     },
-    picture: { type: String,
-    default: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fstock.adobe.com%2Fde%2Fsearch%3Fk%3Ddefault%2Bprofile%2Bpicture&psig=AOvVaw1DVoOYFMVv72l8Qcfo87HC&ust=1644938812129000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCNjVyd7A__UCFQAAAAAdAAAAABAD'
+    picture: { 
+        type: String,
+        default: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fstock.adobe.com%2Fde%2Fsearch%3Fk%3Ddefault%2Bprofile%2Bpicture&psig=AOvVaw1DVoOYFMVv72l8Qcfo87HC&ust=1644938812129000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCNjVyd7A__UCFQAAAAAdAAAAABAD'
     },
     googleId: {
         type: String,
@@ -22,7 +22,19 @@ const userSchema = new mongoose.Schema({
     },
     password: {
         type: String,
+        required: true,
+        minLength: 8,
     },
+    createdAt: { 
+        type: Date,
+        default: Date.now,
+        immutable: true,
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now,
+        immutable: true,
+    }
 })
 
 module.exports = mongoose.model('User', userSchema)
