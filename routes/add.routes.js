105407eaccd914fba2d0037ce949d4db827c3340
alohacyclist <c-mueller87@live.de>
2022-02-26 11:43:12 +0100

    add weather api, add map location

--- routes/add.routes.js
+++ routes/add.routes.js
@@ -1,21 +1,28 @@
 const router = require('express').Router();
+var axios = require("axios").default;
 const Place = require('../models/places.model')
 const Cities = require('../models/cities.model')
-
-var axios = require("axios").default;
+const multer  = require('multer')
+const upload = require('../config/cloudstorage')
 const dbDetails = require('../db.json')
+const {isLoggedIn} = require('../middlewares/guard')
 
-router.post('/city/:id/add-place', async (req, res) => {
+router.post('/city/:id/add-place', isLoggedIn, upload.single('image'), async (req, res) => {
     const city = await Cities.findById(req.params.id)
-    const place = await Place.create({ ...req.body })
-    city.places.push(place.id)
+    if(!req.file) {const place = await Place.create({ ...req.body, author: req.session.currentUser}); city.places.push(place.id); 
     try {
         await place.save()
         await city.save()
     } catch (err) {
         console.error(err)
-    }
-    
+    }}
+    else {const place = await Place.create({ ...req.body, image: req.file.path, author: req.session.currentUser}); city.places.push(place.id);
+    try {
+        await place.save()
+        await city.save()
+    } catch (err) {
+        console.error(err)
+    }}    
     res.redirect(`/city/${city.id}`)
 })
 
--- routes/search.routes.js
+++ routes/search.routes.js
@@ -1,5 +1,7 @@
 const router = require('express').Router()
 const Cities = require('../models/cities.model')
+const Map = require('../models/map.model')
+const axios = require('axios')
 
 router.post('/search', async (req, res) => {
     const result = await Cities.find({
@@ -18,7 +20,33 @@ router.get('/:country', async (req, res) => {
 
 router.get('/city/:id', async (req, res) => {
     const result = await Cities.findById(req.params.id).populate('places')
-    res.render('cities/city-info', { result })
+    
+    // GET WEATHER
+    const city = result.name
+    const weatherOptions = {
+    method: 'GET',
+    url: 'https://weatherapi-com.p.rapidapi.com/forecast.json',
+    params: {q: `${city}`, days: '3'},
+    headers: {
+    'x-rapidapi-host': 'weatherapi-com.p.rapidapi.com',
+    'x-rapidapi-key': process.env.RAPID_API_KEY
+        }
+    }
+
+    const response = await axios.request(weatherOptions)
+
+    const locationOptions = {
+        method: 'GET',
+        url: `https://api.myptv.com/geocoding/v1/locations/by-text?searchText=${city}`,
+        headers: { 'apiKey': 'MjYxMTAxZjJkODc3NDhmMmIwZTYwMGQyZGQ2YTdkZWM6NWI1ODQ2MDgtODBlYi00NmYxLWJiNDEtOGZkMzI5MTMyNTlk', "Content-Type": "application/json"  }
+        }
+
+    const locationResponse = await axios.request(locationOptions)
+     
+    const coordinates = [locationResponse.data.locations[0].referencePosition.latitude, locationResponse.data.locations[0].referencePosition.longitude]
+
+    res.render('cities/city-info', { result, response, coordinates })
+    
 })
 
 module.exports = router
